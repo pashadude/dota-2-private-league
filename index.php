ad556dd8948ec2e4634a298e5c78d1b5573c4674
pashadude <pavel.dudko@gmail.com>
2014-09-30 18:51:25 -0500

    Stats counting based on last 100 games added

--- index.php
+++ index.php
@@ -30,20 +30,54 @@ function dotasname($dota_id) {
 	return $name;
 }
 
-function getLastMatches ($dota_id){
+function countDotaStatsByMatches ($dota_id){
 	$matches_mapper_web = new matches_mapper_web();
 	$matches_mapper_web->set_account_id($dota_id);
 	$matches_short_info = $matches_mapper_web->load();
+	$results = array();
+	$k = 1;
+	$results['gold_per_min'] = 0;
+	$results['xp_per_min'] = 0;
 	foreach ($matches_short_info AS $key=>$match_short_info) {
 	    $match_mapper = new match_mapper_web($key);
 	    $match = $match_mapper->load();
-	    $mm = new match_mapper_db();
-	    $mm->save($match);
-	    //print_r($mm);
+	    $slots = $match->get_all_slots();
+	    	foreach ($slots as $slot) {
+	    		$steam_id = (player::convert_id($slot->get('account_id')));
+	    		if ($steam_id == $dota_id) {
+	    			$results['kills'] += $slot->get('kills');
+	    			$results['deaths'] += $slot->get('deaths');
+	    			$results['assists'] += $slot->get('assists');
+	    			//echo $slot->get('gold_per_min')."-gold".$slot->get('xp_per_min')."-exp";
+	    			$results['gold_per_min'] = ($results['gold_per_min']*($k-1) + $slot->get('gold_per_min'))/$k;
+	    			$results['xp_per_min'] = ($results['xp_per_min']*($k-1) + $slot->get('xp_per_min'))/$k;
+					if ($match->get('radiant_win')) {
+						if ($slot->get('player_slot')<128) {
+							$results['wins']+=1;
+						} else {
+							$results['losses']+=1;
+						}
+					} else {
+						if ($slot->get('player_slot')<128) {
+							$results['losses']+=1;
+						} else {
+							$results['wins']+=1;
+						}
+					}
+	    		}
+	    	}
+	    $k++;
 	}
+
+	return $results;
 }
 
-function getMatchPlayers ($slots, $players, $teamname){
+function checkMatchPlayers ($team, $match){
+	
+}
+
+
+function getMatchTeamPlayers ($slots, $players, $teamname){
 	for ($i=0; $i < 5; $i++) { 
 		$steam_id = player::convert_id($slots[$teamname][$i]->get('account_id'));		
 		if($steam_id != player::ANONYMOUS) {
@@ -77,11 +111,11 @@ function getMatchResults ($match_id){
 	$results = array ();
 
     if($match->get('radiant_win')){
-		$results['winners'] = getMatchPlayers ($slots, $players, 'radiant');
-		$results['losers'] = getMatchPlayers ($slots, $players, 'dire');
+		$results['winners'] = getMatchTeamPlayers ($slots, $players, 'radiant');
+		$results['losers'] = getMatchTeamPlayers ($slots, $players, 'dire');
 	} else {
-		$results['winners'] = getMatchPlayers ($slots, $players, 'dire');
-		$results['losers'] = getMatchPlayers ($slots, $players, 'radiant');
+		$results['winners'] = getMatchTeamPlayers ($slots, $players, 'dire');
+		$results['losers'] = getMatchTeamPlayers ($slots, $players, 'radiant');
 	}
 	
 	return $results;
@@ -97,11 +131,14 @@ function getMatchResults ($match_id){
 //one alias
 //echo dotasname(steamid2dotaid('pashadudue'));
 
+//statistics by player
+print_r (countDotaStatsByMatches(steamid2dotaid('pashadudue')));
 
 
 
+//all_league_matchez
 
-//getLastMatches ($doter);
+//matchez of certain league, certain date, certain users
 
 
 
