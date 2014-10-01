4fc9e8f3192f825a3f08562bc1c4509c88545dfc
pashadude <pavel.dudko@gmail.com>
2014-10-01 13:22:02 -0500

    added exceptions

--- index.php
+++ index.php
@@ -34,42 +34,45 @@ function countDotaStatsByMatches ($dota_id){
 	$matches_mapper_web = new matches_mapper_web();
 	$matches_mapper_web->set_account_id($dota_id);
 	$matches_short_info = $matches_mapper_web->load();
-	$results = array();
-	$k = 1;
-	$results['gold_per_min'] = 0;
-	$results['xp_per_min'] = 0;
-	foreach ($matches_short_info AS $key=>$match_short_info) {
-	    $match_mapper = new match_mapper_web($key);
-	    $match = $match_mapper->load();
-	    $slots = $match->get_all_slots();
-	    	foreach ($slots as $slot) {
-	    		$steam_id = (player::convert_id($slot->get('account_id')));
-	    		if ($steam_id == $dota_id) {
-	    			$results['kills'] += $slot->get('kills');
-	    			$results['deaths'] += $slot->get('deaths');
-	    			$results['assists'] += $slot->get('assists');
-	    			//echo $slot->get('gold_per_min')."-gold".$slot->get('xp_per_min')."-exp";
-	    			$results['gold_per_min'] = ($results['gold_per_min']*($k-1) + $slot->get('gold_per_min'))/$k;
-	    			$results['xp_per_min'] = ($results['xp_per_min']*($k-1) + $slot->get('xp_per_min'))/$k;
-					if ($match->get('radiant_win')) {
-						if ($slot->get('player_slot')<128) {
-							$results['wins']+=1;
+	if (empty( $matches_short_info ) ) {  
+	echo "the user has not shared matches";
+	} else {
+		$results = array();
+		$k = 1;
+		$results['gold_per_min'] = 0;
+		$results['xp_per_min'] = 0;
+		foreach ($matches_short_info AS $key=>$match_short_info) {
+		    $match_mapper = new match_mapper_web($key);
+		    $match = $match_mapper->load();
+		    $slots = $match->get_all_slots();
+		    	foreach ($slots as $slot) {
+		    		$steam_id = (player::convert_id($slot->get('account_id')));
+		    		if ($steam_id == $dota_id) {
+		    			$results['kills'] += $slot->get('kills');
+		    			$results['deaths'] += $slot->get('deaths');
+		    			$results['assists'] += $slot->get('assists');
+		    			//echo $slot->get('gold_per_min')."-gold".$slot->get('xp_per_min')."-exp";
+		    			$results['gold_per_min'] = ($results['gold_per_min']*($k-1) + $slot->get('gold_per_min'))/$k;
+		    			$results['xp_per_min'] = ($results['xp_per_min']*($k-1) + $slot->get('xp_per_min'))/$k;
+						if ($match->get('radiant_win')) {
+							if ($slot->get('player_slot')<128) {
+								$results['wins']+=1;
+							} else {
+								$results['losses']+=1;
+							}
 						} else {
-							$results['losses']+=1;
+							if ($slot->get('player_slot')<128) {
+								$results['losses']+=1;
+							} else {
+								$results['wins']+=1;
+							}
 						}
-					} else {
-						if ($slot->get('player_slot')<128) {
-							$results['losses']+=1;
-						} else {
-							$results['wins']+=1;
-						}
-					}
-	    		}
-	    	}
-	    $k++;
+		    		}
+		    	}
+		    $k++;
+		}
+		return $results;
 	}
-
-	return $results;
 }
 
 function checkMatchPlayers ($team, $match){
@@ -123,7 +126,10 @@ function getMatchResults ($match_id){
 
 
 //convertor
-//$doter = steamid2dotaid('pashadudue');
+// steamid2dotaid('pys_paul');
+
+
+
 
 //match results
 //print_r (getMatchResults(37626434));
@@ -132,7 +138,7 @@ function getMatchResults ($match_id){
 //echo dotasname(steamid2dotaid('pashadudue'));
 
 //statistics by player
-print_r (countDotaStatsByMatches(steamid2dotaid('pashadudue')));
+print_r (countDotaStatsByMatches(steamid2dotaid('pys_paul')));
 
 
 
